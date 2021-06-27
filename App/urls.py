52b58c014c87c27816cb7c531119cbdc249c23e1
Abhinandan Shrestha <shtabhi@gmail.com>
2021-06-27 13:44:27 +0545

    done

---
+++ App/urls.py
@@ -0,0 +1,15 @@
+from . import views
+from django.urls import path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+  path('',views.index, name="index"),
+  path('blogs/', views.PostList.as_view(), name="blog"),
+  path('<slug:slug>/', views.DetailView.as_view(), name="post_detail"),
+]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
