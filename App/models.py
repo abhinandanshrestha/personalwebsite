a400c340e8c31a5e3b3db47b1f3b7b8d5833fd06
Abhinandan Shrestha <shtabhi@gmail.com>
2021-06-27 14:40:13 +0545

    asdasdasd

--- App/models.py
+++ App/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
   title =  models.CharField(max_length=200, unique=True)
   slug =   models.SlugField(max_length=200, unique=True)
   author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-  image = models.ImageField(upload_to='images/')
+  image = models.ImageField(upload_to='https://portfolioabhinandan-cli.herokuapp.com/images/')
   created_on = models.DateTimeField(auto_now_add=True)
   updated_on = models.DateTimeField(auto_now=True)
   content = models.TextField()
